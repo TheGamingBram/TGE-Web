c171375d323b8c34d5972ad9efc1ddb8a38268e9
TheGamingBram <bramhesen2002@gmail.com>
2022-01-20 14:23:37 +0100

    yes

--- Scripts/script.js
+++ Scripts/script.js
@@ -26,25 +26,4 @@ var particles = Particles.init({
 });
 
 
-const d = new Date();
-let month = d.getUTCMonth(); // 0 = Jan, 1 = Feb, 2 = Mar, 3 = Apr, 4 = May, 5 = Jun, 6 = Jul, 7 = Aug, 8 = Sep, 9 = Oct, 10 = Nov, 11 = Dec
-// console.log(month);
-obje = document.getElementById('Background');
-
-titleobjects = document.getElementsByClassName('loader');
-for(var i = 0; i < titleobjects.length; i++){
-  titleobjects[i].style.display = 'none';
-}
-
-
-
-if(month == 11){
-  obje.style.backgroundImage = "url('https://i.imgur.com/mPDNe0D.jpg')";
-  document.getElementById('xmas').style.display = 'block';
-}else if(month == 9){
-  obje.style.backgroundImage = "url('https://i.imgur.com/BRML8Jo.jpg')";
-  document.getElementById('spook').style.display = 'block';
-}else{
-  document.getElementById('title').style.display = 'block';
-}
 
